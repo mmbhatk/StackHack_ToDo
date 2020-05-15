379f3fa942e128baa60856142bbdd9e9b608504a
Lohith N V <n.vlohithnv@gmail.com>
2020-05-16 00:44:41 +0530

    mock ToDo app with backend integration

--- apps/todos/src/app/app.component.ts
+++ apps/todos/src/app/app.component.ts
@@ -1,13 +1,27 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Message } from '@stack-hack-to-do/api-interfaces';
+import { Todo } from '@stack-hack-to-do/data';
+
 
 @Component({
   selector: 'stack-hack-to-do-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  hello$ = this.http.get<Message>('/api/hello');
-  constructor(private http: HttpClient) {}
+  todos: Todo[] = [];
+
+  constructor(private http: HttpClient) {
+    this.fetch();
+  }
+
+  fetch() {
+    this.http.get<Todo[]>('/api/todos').subscribe(t => (this.todos = t));
+  }
+
+  addTodo() {
+    this.http.post('/api/addTodo', {}).subscribe(() => {
+      this.fetch();
+    });
+  }
 }
