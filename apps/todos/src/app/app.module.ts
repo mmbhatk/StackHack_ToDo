98e1f9b316f750ab55a6314c85f075c4bfbdb29c
Lohith N V <n.vlohithnv@gmail.com>
2020-05-16 14:50:39 +0530

    added sidenav

--- apps/todos/src/app/app.module.ts
+++ apps/todos/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatIconModule} from '@angular/material/icon';
+import {MatListModule} from '@angular/material/list';
+import {MatButtonModule} from '@angular/material/button';
+
+
+
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, HttpClientModule],
+  imports: [BrowserModule, HttpClientModule, BrowserAnimationsModule, MatToolbarModule, MatSidenavModule, MatIconModule, MatListModule, MatButtonModule],
   providers: [],
   bootstrap: [AppComponent],
 })
