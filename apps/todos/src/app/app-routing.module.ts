ef2b1c668d9262fd5bbee754165e55b2b519bb8e
mmbhatk <kmanasvibhat@gmail.com>
2020-06-07 16:53:19 +0530

    Removed CalendarComponent

--- apps/todos/src/app/app-routing.module.ts
+++ apps/todos/src/app/app-routing.module.ts
@@ -1,11 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { TaskHandlerComponent } from './task-handler/task-handler.component';
-import { CalendarComponent } from './calendar/calendar.component'
 
 const routes: Routes = [
   { path: 'dashboard', component: TaskHandlerComponent },
-  { path: 'calendar', component: CalendarComponent },
   { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
 ];
 
